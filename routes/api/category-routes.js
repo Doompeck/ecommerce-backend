9cacf2c58ef77c80cb19d62474f1a6ecad404ae5
Benjamin <bstanf@uw.edu>
2022-08-22 18:18:59 -0700

    Initial commit

---
+++ routes/api/category-routes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+    // Category.findAll()
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  // Category.findByPK(req.params.id)
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+  //Category.create(req.body)  
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+  // Category.update(req.body, {
+  // where: { id: req.params.id}
+  // })
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  // Category.destroy(req.body, {
+  //   where: { id: req.params.id}
+  // })
+});
+
+module.exports = router;

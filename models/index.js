5ba202c218f355db7f0eebb3f35ed1f2698b319d
Benjamin <bstanf@uw.edu>
2022-10-02 20:58:48 -0700

    updated model index to add my relations

--- models/index.js
+++ models/index.js
@@ -8,20 +8,28 @@ const ProductTag = require('./ProductTag');
 // MANY TO ONE will have a foreign key in the product referencing the category_id
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  as: 'productCategory'
+  // as: 'productCategory'
 });
 // Categories have many Products
 // ONE TO MANY
 Category.hasMany(Product, {
   foreignKey: 'category_id',
   // sets the category to null within each product
-  OnDelete: 'SET NULL',
-  as: 'CategoryProducts'
+  // OnDelete: 'SET NULL',
+  // as: 'CategoryProducts'
 });
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
+
 
 module.exports = {
   Product,

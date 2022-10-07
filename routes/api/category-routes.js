a9475eafb362c9f866dad6a36055ba3e72ce22d9
Benjamin <bstanf@uw.edu>
2022-10-06 19:45:57 -0700

    succesful database build and seed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,36 +3,39 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
     // Category.findAll()
     const categories = await Catergory.findAll ({
-      include: Product
+      include: {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
     });
     
     res.json(categories);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   // Category.findByPK(req.params.id)
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   //Category.create(req.body)  
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   // Category.update(req.body, {
   // where: { id: req.params.id}
   // })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   // Category.destroy(req.body, {
   //   where: { id: req.params.id}

74268b7d650cb68dd1c668f1fbbd2f0e9c796431
Benjamin <bstanf@uw.edu>
2022-10-06 21:24:36 -0700

    all category routes functional

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,6 +45,12 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   // create a new category
   //Category.create(req.body)
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put("/:id", async (req, res) => {
@@ -52,6 +58,16 @@ router.put("/:id", async (req, res) => {
   // Category.update(req.body, {
   // where: { id: req.params.id}
   // })
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
@@ -59,6 +75,20 @@ router.delete("/:id", async (req, res) => {
   // Category.destroy(req.body, {
   //   where: { id: req.params.id}
   // })
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "no Category found with this id." });
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
